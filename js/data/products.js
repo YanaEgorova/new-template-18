21ffaf0726ce79f5d3d44664c4995bfa98cc5b7a
Yana <yanaegorova69.3@gmail.com>
2021-08-29 19:40:52 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,169 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Suction Holder for Sponges',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Sink storage',
+                text: 'Kitchen sink basket holds sponges, scrubbers, scouring pads, bars of soap, drain stoppers, razors, washcloths, and more! Great for kitchen, workplace, bathroom, craft room, classroom, or office sinks for extra storage wherever you need.',
+            },
+            {
+                title: 'Functional design',
+                text: 'Open design allows water to drain, letting sponges and scrubbers dry.',
+            },
+            {
+                title: 'Durable',
+                text: 'Rust-resistant steel with matte black finish provides years of quality use.',
+            },
+            {
+                title: 'Reliable suction',
+                text: 'Dual plastic suction cups easily stick to smooth surfaces for convenient drainage.',
+            },
+            {
+                title: 'Ideal size',
+                text: 'Sponge basket measures 5.75" x 2.25" x 2.5", perfect for your sink.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.jpg', './img/img1-4.jpg'],
+    },
+    {
+        id: 'product-2',
+        name: 'Baseball set for kids',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Totsports T-ball set',
+                text: 'Searching for an indoor or outdoor game that will teach your child how to play baseball, softball, or Wiffle ball. These amazing baseball toys are a good start that will give excitement to your kids as they learn sports.',
+            },
+            {
+                title: 'Toddler developing skills',
+                text: 'This tball set has an adjustable height "T" which adapts to a child\'s developing motor skills, batting skills, and hand-eye coordination. By adding this to your toddler sports toys, your kids are on their way to be a pro.',
+            },
+            {
+                title: 'Adjustable kids tee ball set',
+                text: 'Toddlers of different height, alone or with their playmates, can play. Engage in experiencing success at their age. Add excitement to your normal bat n ball activity, whether by swing, batting, and hitting tee baseball.',
+            },
+            {
+                title: 'Easy to use',
+                text: 'The set comes with 5 plastic tee balls and has an oversized plastic baseball bat so everyone can hit out of their backyard easily. Boys and girls can be part of the batting tee ball action and enjoy the imaginative roar of the crowd.',
+            },
+            {
+                title: 'Convenient storage',
+                text: 'When home runs are over and everyone has been strike-out, your baseball toy can be conveniently stored. This lightweight plastic t ball set has a special hang-on-the-wall design for easy small-space storage indoor or outdoor.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png' , './img/img2-3.png', './img/img2-4.png', './img/img2-5.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Cat Condo Tree Tower',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Elevated cat hammock with dual scratching post pillars; durable, long-lasting construction.',
+            },
+            {
+                text: 'Plush hammock provides a comfortable space for your cat to relax.',
+            },
+            {
+                text: 'Helps keep your cat from damaging carpets, furniture, curtains, and more.',
+            },
+            {
+                text: 'Natural jute fiber scratching posts help keep nails healthy.',
+            },
+            {
+                text: 'Neutral color tones fit in with your home’s existing décor.',
+            },
+            {
+                text: 'Dimensions: 15.75 x 15.75 x 19.69 inches; 8.95 pounds',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Portable Denture Cleaner',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Portable true ultrasonic denture/aligner/retainer cleaner. AC100-240V or DC12V can be used worldwide.',
+            },
+            {
+                text: 'DC12V 25W, Sweeping frequency 36, 000-41, 000 Hz. This is the first DC powered ultrasonic cleaner that surpasses 20W power. A technology breakthrough.',
+            },
+            {
+                text: 'Tanks size Φ3”x1. 9” (Φ78x48 mm). Tank is bigger than our popular F3900 and fits regular sized dental and sleep apnea appliances.',
+            },
+            {
+                text: '25W which is 25% more than F3900, plus full wave sweeping frequency. Therefore its cleaning efficiency is more than doubled than F3900.',
+            },
+            {
+                text: 'Touch-sensing control, 3-minute auto shutoff, detachable power cord. Strainer attached to the lid for jewelry. Easy to use.',
+            },
+            {
+                text: 'To charge the battery, make sure to turn the switch to On position. Turn it off when it is not in use.',
+            },
+            {
+                text: 'For extra larze size dentures, appliances or joined appliances, model D3000 or D2830 is sized properly.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Ottoman',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Tufted Top.',
+            },
+            {
+                text: 'Metal Frame.',
+            },
+            {
+                text: 'Easy Assembly.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Water Cooler Dispenser',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Top-loading dispenser',
+                text: 'Features an easy-to-use top-loading water system, which allows you to see if the water is running low. Suitable for 3 and 5 gallon water bottles (not included).',
+            },
+            {
+                title: 'Innovative design',
+                text: 'Free standing slim, white space-saving design is ideal for any space – making it perfect for homes, kitchens, offices, dorm rooms & more!',
+            },
+            {
+                title: 'Hot & cold water',
+                text: 'When you use our water dispenser, choose between ice-cold and steaming hot water – perfect for cool, refreshing drinks and tea, coffee, hot cocoa, instant soup & more!',
+            },
+            {
+                title: 'Safe & easy dispensing',
+                text: 'Two push buttons and two separate spigots for both cold and hot water make dispensing easy and allow you to refill almost any sized cup, hassle-free. Hot water dispenser has a child safety lock, preventing any accidental pouring.',
+            },
+            {
+                title: 'Removable drip tray',
+                text: 'Water dispenser is equipped with a removable drip tray, allowing for a quick and easy clean up!',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png'],
+    },
+]
